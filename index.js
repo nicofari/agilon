06d616cd1e1e8f5ff8ef345b474c42700278eb11
nik <nicola.farina@teletu.it>
2015-11-29 10:51:00 +0100

    Following the tutorial: added cool-ascii-faces

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
@@ -9,12 +10,14 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function (request, response) {
+    response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get('/cool', function (request, response) {
+    response.send(cool());
 });
 
-
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
